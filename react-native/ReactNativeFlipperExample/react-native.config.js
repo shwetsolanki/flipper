bb9b6605f7956b9cdb6faa1b46cfe85d77a15257
Shwet Solanki <shwetsolanki@gmail.com>
2021-09-04 15:44:27 +0530

    Adds Amazon FlingSDK to reproduce crash while flipper is open

---
+++ react-native/ReactNativeFlipperExample/react-native.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    dependencies: {
+        'react-native-firetv': {
+            platforms: {
+                ios: {
+                    sourceDir: './packages/react-native-firetv'
+                }
+            }
+        }
+    }
+}
